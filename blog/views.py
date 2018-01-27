c9ef473aa1375ec5a404cefe67342c7ecec5d958
clementsteible <c.steible@hotmail.fr>
2018-01-27 17:31:35 +0100

    1ère QuerySet intégrer au HTML

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    tri_creations_recentes = Post.objects.filter(published_date__lte=timezone.now()).order_by("created_date")
+    return render(request, 'blog/post_list.html', {"tri_creations_recentes" : tri_creations_recentes})

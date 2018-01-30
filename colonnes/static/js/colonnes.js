f51433d665c7b17592a31ddcd33ba5784fe3018f
clementsteible <c.steible@hotmail.fr>
2018-01-30 15:18:15 +0100

    Ajouts
    - template html global ajouté : logo "ebeck", boutons du menu (images utilisées stockées dans "static/css/images")
    - modifications au niveau du CSS (redimensionnement des logos et des boutons)
    - ajout de fichiers pour le JS
    - ajout de fonction pour les boutons (les fonctions sont encore vides)

---
+++ colonnes/static/js/colonnes.js
@@ -0,0 +1,24 @@
+//Amène à la page permettant l'ajout d'une nouvelle entrée pour les colonnes de Beck
+function nouvelle(){
+
+}
+
+//Amène à la page Journal (historique des colonnes de Beck déjà entrées)
+function journal(){
+
+}
+
+//Amène à la page des Statistiques
+function statistiques(){
+
+}
+
+//Amène à la page des conseils de développement personnel
+function developpement(){
+
+}
+
+//Amène à la page des paramètres
+function parametres(){
+
+}

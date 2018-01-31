a5e29c7aa9b7b7dcb56d11a23ee8a5de6ca1abb3
SorrowSevlow <35839892+SorrowSevlow@users.noreply.github.com>
2018-01-31 23:43:51 +0100

    Commit pour pull encore

--- colonnes/views.py
+++ colonnes/views.py
@@ -1,47 +1,27 @@
 from django.shortcuts import render
 from colonnes.models import Personne
 from .forms import InscriptionForm, AuthentificationForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def deverouillage(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #context = {'latest_question_list': latest_question_list}
-    return render(request, 'colonnes/deverouillage.html', {})#context)
+    return render(request, 'colonnes/deverouillage.html', {})
 
 def nouvelle_entree(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #context = {'latest_question_list': latest_question_list}
     return render(request, 'colonnes/nouvelle_entree.html', {})
 
 def journal(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #context = {'latest_question_list': latest_question_list}
     return render(request, 'colonnes/journal.html', {})
 
 def statistiques(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #context = {'latest_question_list': latest_question_list}
     return render(request, 'colonnes/statistiques.html', {})
 
 def developpement_personnel(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #context = {'latest_question_list': latest_question_list}
-    georges_pays = Personne.objects.get(id=1).pays
-    context = {'georges_pays': georges_pays}
-    return render(request, 'colonnes/developpement_personnel.html', context)
+    return render(request, 'colonnes/developpement_personnel.html', {})
 
 def parametres(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #context = {'latest_question_list': latest_question_list}
     return render(request, 'colonnes/parametres.html', {})
-
 
 def authentification(request):
     #On ajoute le formulaire d'authentification à la vue
@@ -49,6 +29,14 @@ def authentification(request):
     return render(request, 'colonnes/authentification.html', {'formAuthentification':formAuthentification})
 
 def inscription(request):
-    #On ajoute le formulaire d'inscription à la vue
-    formInscription = InscriptionForm()
+    formAuthentification = AuthentificationForm()
+    if request.method == "POST":
+        formInscription = InscriptionForm(request.POST)
+        if formInscription.is_valid():
+            personne = formInscription.save(commit=False)
+            personne.save()
+            return render(request, 'colonnes/authentification.html',  {'formAuthentification':formAuthentification})
+    else :
+        #On ajoute le formulaire d'inscription à la vue
+        formInscription = InscriptionForm(request.POST)
     return render(request, 'colonnes/inscription.html', {'formInscription':formInscription})

de98fbc96a653f300d9390c1fe6996438b54ac4e
clementsteible <c.steible@hotmail.fr>
2018-02-01 00:14:37 +0100

    Bug

--- colonnes/forms.py
+++ colonnes/forms.py
@@ -2,13 +2,11 @@ from django import forms
 from .models import Personne
 
 class InscriptionForm(forms.ModelForm):
-
     class Meta:
         model = Personne
         fields = ('nom', 'prenom', 'date_de_naissance', 'pays', 'ville', 'langue', 'adresse_mail','mot_de_passe',)
 
 class AuthentificationForm(forms.ModelForm):
-
     class Meta:
         model = Personne
         fields = ('adresse_mail','mot_de_passe',)

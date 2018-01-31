a5eaf772c5efb889e4a7400ec8171da0fd33f4ce
Mohamedmb <mohamedmbentayeb@gmail.com>
2018-01-31 10:46:21 +0100

    Models Provisoires
    Colonne, Emotion, Tag,  LiensTagColonne, Conseil,

--- colonnes/models.py
+++ colonnes/models.py
@@ -18,6 +18,7 @@ from django.utils import timezone
 """
 
 class Personne(models.Model):
+
     UTILISATEUR = 'UT'
     THERAPEUTE = 'TH'
     STATUT_CHOICES = (
@@ -37,3 +38,49 @@ class Personne(models.Model):
     ville = models.CharField(max_length=30, help_text = "Ville")
     langue = models.CharField(max_length=30, help_text = "Nom")
     adresse_mail = models.EmailField(max_length=254, blank=False, help_text = "Nom")
+
+
+
+"""
+
+class Colonne(models.model):
+
+        situation = models.TextField(max_length=30, blank=False, help_text = "Décrivez la Situation")
+        pensée_aut = models.TextField(max_length=30, blank=False, help_text = "Nom")
+        pensée-alt = models.TextField(max_length=30, blank=False, help_text = "Nom")
+        date_ajout = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        horaire_ajout = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        date_event = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        horaire_event = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        id_ut = models.CharField(max_length=30, blank=False, help_text = "Nom")
+
+class Emotion(models.model):
+
+        id_col = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        id_tag = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        statut_emo = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        intensite = models.IntegerField(help_text = "Intensité de l'émotion")
+    
+    
+    
+    
+class Tag(models.model):
+
+        id_ut = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        id_col = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        nom_tag = models.CharField(max_length=30, blank=False, help_text = "Entrez un Tag")
+        type_tag = models.CharField(max_length=30, blank=False, help_text = "Nom")
+    
+
+ class Lien_tag_colonne(models.model):
+
+        id_tag = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        id_col = models.CharField(max_length=30, blank=False, help_text = "Nom")
+
+class Conseil(models.model):
+
+        type_conseil = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        contenu_conseil = models.CharField(max_length=30, blank=False, help_text = "Nom")
+        
+                
+        """

8a2ff4f981eb1c519e5d6689a5ec86593160037b
SorrowSevlow <35839892+SorrowSevlow@users.noreply.github.com>
2018-02-01 17:00:09 +0100

    Reglage du problème de redirection
    Il y avait un problème de redirection des pages vers login ou loggout

--- colonnes/urls.py
+++ colonnes/urls.py
@@ -4,8 +4,10 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='colonnes/login.html')),
-    path('logout/', auth_views.LogoutView.as_view(template_name='colonnes/logged_out.html')),
+    #Page de login
+    path('login/', auth_views.LoginView.as_view(template_name='colonnes/login.html'), name='login'),
+    #Page de logout
+    path('logout/', auth_views.LogoutView.as_view(template_name='colonnes/logged_out.html'), name='logout'),
     #Menu principal de l'application web
     path('', views.deverouillage, name='deverouillage'),
     #Page permettant d'ajouter une nouvelle situation complète
